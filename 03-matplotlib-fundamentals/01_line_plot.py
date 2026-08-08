1b81b26bbf0583b6cef5c273f2ae9a15a42a2cd1
mosiinmushtaq70-a11y <mosiinmushtaq70@gmail.com>
2026-08-08 16:39:14 +0530

    Two Graphs in one FIle
    Made two graphs in one code file and  learnt plt.clf() to clear the canvas

--- 03-matplotlib-fundamentals/01_line_plot.py
+++ 03-matplotlib-fundamentals/01_line_plot.py
@@ -13,3 +13,18 @@ plt.title("Just to see")
 # plt.show()  There was some problem as vscode terminal cant show the graph , so imma use the savefig function instead
 plt.savefig("01_line_plot.png")
 
+##ONE IMPORTANT THING I LEANT  is that if i dont clear the canvas , the first graph will be wrapped by the first graph 
+# the function to clear the canvas is 
+
+plt.clf()
+
+days = [1, 2, 3, 4, 5, 6, 7]
+cloud_coverage = [45, 60, 30, 80, 25, 50, 35] 
+
+plt.plot(days,cloud_coverage)
+
+plt.xlabel("Days")
+plt.ylabel("Cloud Coverage (%)")
+plt.title("Cloud Coverage Over Time")
+plt.savefig("01_line_plot(1).png")
+
