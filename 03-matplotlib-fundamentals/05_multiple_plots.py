ef966c84a81f521f91133f0fc50642e72b0868c3
mosiinmushtaq70-a11y <mosiinmushtaq70@gmail.com>
2026-08-08 17:32:14 +0530

    Add Multiple plots
    Learned multiplr plots in one graph.
    subplot is wrong while as
    subplots() is correct
    and also while naming indivisual plot, title is given by axes[0].set_title("Title here")

--- 03-matplotlib-fundamentals/05_multiple_plots.py
+++ 03-matplotlib-fundamentals/05_multiple_plots.py
@@ -1,1 +1,23 @@
-import 
+import matplotlib.pyplot as plt
+
+
+fig, axes=plt.subplots(1,3) # subplots is important - donot use subplot
+                               # axes is just a name 
+x1=[1,2,3,5,6]
+y1=[10,20,30,40,50]
+
+x2 = [0.3, 0.6, 0.8, 0.4, 0.9, 0.5, 0.7]
+y2 = [80, 60, 20, 75, 10, 65, 30]
+
+y3=[29,45,78,32,90]
+x3=["Landsat","Sentinel","ESA","NASA","ISRO"]
+
+
+axes[0].plot(x1, y1)
+axes[0].set_title("Plot 1")
+axes[1].scatter(x2, y2)
+axes[1].set_title("Plot 2")
+axes[2].bar(x3, y3)
+axes[2].set_title("Plot 3")
+
+plt.savefig("05_multiple_plots.png")
