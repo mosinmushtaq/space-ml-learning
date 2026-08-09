fb3aec2e9a3e7b829efc907a64a9b073e0d1f695
mosinmushtaq <mosiinmushtaq70@gmail.com>
2026-08-09 22:58:46 +0530

    Added a small logic

--- 03-matplotlib-fundamentals/09_styled_subplots.py
+++ 03-matplotlib-fundamentals/09_styled_subplots.py
@@ -5,7 +5,9 @@ sentinel = [45, 60, 30, 80, 25, 50, 35]
 landsat = [48, 58, 32, 78, 28, 52, 38]
 goes = [40, 65, 28, 82, 20, 55, 32]
 temperature = [22, 24, 19, 25, 23, 26, 20]
-combined=[45, 60, 30, 80, 25, 50, 35,48, 58, 32, 78, 28, 52, 38,40, 65, 28, 82, 20, 55, 32]
+
+# combined=[45, 60, 30, 80, 25, 50, 35,48, 58, 32, 78, 28, 52, 38,40, 65, 28, 82, 20, 55, 32]
+combined=sentinel+landsat+goes
 fig, axes=plt.subplots(1,3)
 
 axes[0].plot(days,sentinel,color="red",linestyle="-",marker="o",label="Sentinel-2:")
