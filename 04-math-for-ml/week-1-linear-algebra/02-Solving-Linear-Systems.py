10dc880a5391accc3dd8a8654b553d0bb9adf239
mosiinmushtaq70-a11y <mosiinmushtaq70@gmail.com>
2026-08-12 11:59:34 +0530

    Add np.linalg.solve() function

--- 04-math-for-ml/week-1-linear-algebra/02-Solving-Linear-Systems.py
+++ 04-math-for-ml/week-1-linear-algebra/02-Solving-Linear-Systems.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 # so in order to solve a system of linear ewuations we have a funtion
-# the function is np.linalg.solve(A,B) which takes two inputs
+    # the function is np.linalg.solve(A,B) which takes two inputs
 
 # A here is the coefficients of x1 and x2 and x3 and so on or a and b and c  so on
 # b here is the result of these equations i.e after the =
-
+                         # This The function works only on square matrices. That is, if the number of equations is 4, the number of variables should also be 4. That is, the system should be complete, non-redundant, and non-contradictory. 
 # e.g -1a + 3b = 7
 #      3a + 2b = 1
 
@@ -12,4 +14,16 @@
 
 # and B will be a 1d array of the constants i.e [7,1]
 
-# this function will 
+# this function will return an array of the resultant values of the variables as [x1,x2,x3]
+
+A=np.array([[-1, 3],
+             [3, 2]], dtype=np.int32)
+
+B=np.array([7, 1], dtype=np.int32)
+
+
+solved=np.linalg.solve(A,B)
+print(solved)
+
+print(f"x1 is : {solved[0]}")
+print(f"x2 is : {solved[1]}")
